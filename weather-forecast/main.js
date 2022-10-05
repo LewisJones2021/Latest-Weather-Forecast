25d906772a858fedda3286cf2b13e092239c845a
Lewis Jones <81636428+LewisJones2021@users.noreply.github.com>
2022-10-05 19:12:53 +0100

    Update main.js

--- weather-forecast/main.js
+++ weather-forecast/main.js
@@ -32,3 +32,14 @@ let weather = {
 document.querySelector('.search button').addEventListener('click', function () {
   weather.search();
 });
+
+document
+  .querySelector('.search-bar')
+  .addEventListener('keyup', function (event) {
+    if (event.key == 'Enter') {
+      weather.search();
+    }
+  });
+
+
+
